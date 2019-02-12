f62a3e17e4ab40ec6b6f153e20573ca362579df0
Mitchell-Childerhose <mitchell.childerhose@uoit.net>
2019-02-12 10:56:14 -0500

    Initial django project w/ user and stock db model

---
+++ simplewealth/main/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+class Users(models.Model):
+	HI = "Hi"
+	MED = "Med"
+	LO = "Lo"
+	RISK_STATUS_CHOICES = (
+		(HI, 'High'),
+		(MED, 'Medium'),
+		(LO, 'Low'),
+	)
+
+	user_id = models.AutoField(primary_key=True)
+	username = models.CharField(max_length=20)
+	first_name = models.CharField(max_length=20)
+	last_name = models.CharField(max_length=20)
+	risk_status = models.CharField(max_length=2,
+		choices = RISK_STATUS_CHOICES, default = MED,
+	)
+	cash_amount = models.FloatField(default=0)
+	sector_breakdown = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.username
+
+class Stocks(models.Model):
+	symbol = models.CharField(primary_key=True, max_length=5)
+	company = models.CharField(max_length=50)
+	current_price = models.FloatField(default=0)
+	change_percentage = models.FloatField(default=0)
+	vwap = models.FloatField(default=0)
+
+	def __str__(self):
+		return self.symbol
